27a063556b3c073ba85af8b3a9bbfcc53e32685f
Jorge Mesa Marrero <jorgemm.dam@gmail.com>
2026-05-27 17:07:10 +0100

    Se implementan los test de la clase #15

---
+++ centroplusconnect/src/test/java/es/ies/puerto/models/ActividadTest.java
@@ -0,0 +1,107 @@
+package es.ies.puerto.models;
+/**
+ * @author AlejandroDonGar y JavierReyPer
+ * @version 1.0.0
+ * 
+ * Clase ActividadTest donde se haran test sobre modelos/Actividades
+ */
+public class ActividadTest {
+
+    // ---- Actividad Constructor ----
+    @Test
+    public void constructorActividadTest0k() {
+    }
+    @Test
+    public void constructorActividadTestNull() {
+    }
+
+    // ---- Actividad ID ----
+    @Test
+    public void getActividadId0k(){
+    }
+    @Test
+    public void getActividadIdNull(){
+    }
+    @Test
+    public void esIntegerValido(){
+    }
+
+    // ---- Actividad Nombre ----
+    @Test
+    public void getActividadNombre0k(){
+    }
+    @Test
+    public void getActividadNombreNull(){
+    }
+    @Test
+    public void esCadenaValida(){
+    }
+
+    // ---- Actividad Tipo ----
+    @Test
+    public void getActividadTipo0k(){
+    }
+    @Test
+    public void getActividadTipoNull(){
+    }
+    @Test
+    public void esTipoActividadValida(){
+    }
+
+    // ---- Actividad Duracion ----
+    @Test
+    public void getActividadDuracion0k(){
+    }
+    @Test
+    public void getActividadDuracionNull(){
+    }
+    @Test
+    public void esIntegerValidoDuracion(){
+    }
+
+    // ---- Actividad Precio ----
+    @Test
+    public void getActividadPrecio0k(){
+    }
+    @Test
+    public void getActividadPrecioNull(){
+    }
+    @Test
+    public void esDoubleValido(){
+    }
+
+    // ---- Actividad Plazas Maximas ----
+    @Test
+    public void getActividadPlazasMaximas0k(){
+    }
+    @Test
+    public void getActividadPlazasMaximasNull(){
+    }
+    @Test
+    public void esIntegerValidoPlazasMaximas(){
+    }
+
+    // ---- Actividad Plazas Ocupadas ----
+    @Test
+    public void getActividadPlazasOcupadas0k(){
+    }
+    @Test
+    public void getActividadPlazasOcupadasNull(){
+    }
+    @Test
+    public void esPlazasOcupadas(){
+    }
+
+    // ---- Actividad ToString ----
+    @Test
+    public void toStringTest0k(){
+    }
+
+    // ---- Actividad Equals ---- 
+    @Test
+    public void equalsTest0K(){
+    }
+    @Test
+    public void equalsTestFalse(){
+    }
+}
